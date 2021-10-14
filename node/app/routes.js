399d22706a8ddf5476409e18e972b2e1514487e1
steven4389 <40493520+steven4389@users.noreply.github.com>
2021-10-13 20:55:56 -0500

    oauth

--- node/app/routes.js
+++ node/app/routes.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // Importar controladores
 const ProductController = require('../app/controllers/ProductController');
+const AuthController = require('../app/controllers/AuthController');
 
 // Products
-router.post('/', ProductController.create);
-router.get('/getByName/:name', ProductController.showByKeyWord);
-router.get('/', ProductController.sowAll);
-router.get('/:id', ProductController.showById);
-router.patch('/:id', ProductController.update);
-router.delete('/:id', ProductController.delete);
+ router.post('/', ProductController.create);
+ router.get('/getByName/:name', ProductController.showByKeyWord);
+ router.get('/', ProductController.sowAll);
+ router.get('/:id', ProductController.showById);
+ router.patch('/:id', ProductController.update);
+ router.delete('/:id', ProductController.delete);
+
+
 
 
 module.exports = router;
