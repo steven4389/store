0c74ea5fe26ec377c0bbd81a9cd0c2d15b13e518
steven4389 <40493520+steven4389@users.noreply.github.com>
2021-10-15 21:16:14 -0500

    admin

--- react/src/components/admin/Admin.js
+++ react/src/components/admin/Admin.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Admin = () => {
+
+    const [users, setusers] = useState('')
+
+    useEffect(() => {
+        getUsersPendientes();
+    }, [])
+
+
+    const getUsersPendientes = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/api/users/pendientes');
+
+            console.log(response.data);
+            setusers(response.data);
+
+        } catch (error) {
+            
+        }
+    }
+
     return (
-        <div>
-            admin
+        <div style={{marginTop:'50px', }}>
+            {users !== '' &&
+                users.map(item=>{
+                    return(
+                        <div style={{display:'inline-flex'}}>
+                            <div style={{marginRight:'10px'}}>{item.email}</div>
+                            <div style={{background:'green', color:'#fff', padding:'5px', borderRadius:'5px'}}>permitir acceso</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
